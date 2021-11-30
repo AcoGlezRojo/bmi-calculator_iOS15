b1af67f71564f9dbacf3c93ec3c01343dff5a07f
AcoGlezRojo <acoidangonzalezrojo@gmail.com>
2021-11-30 17:30:03 +0000

    finalizado

---
+++ BMI Calculator/BMI.swift
@@ -0,0 +1,9 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Acoidán González Rojo on 29/11/21.
+//  Copyright © 2021 Acoidan. All rights reserved.
+//
+
+import Foundation
---
+++ BMI Calculator/BmiBrain.swift
@@ -0,0 +1,9 @@
+//
+//  BmiBrain.swift
+//  BMI Calculator
+//
+//  Created by Acoidán González Rojo on 29/11/21.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
